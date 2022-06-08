24e9b98c0f2857c08c8707cefec80aa40b27f382
Rick Craig <rick@spacervolver.com>
2022-06-08 21:48:15 +0100

    Add docs to one function

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ export const testMe = (a, b) => {
     return a * b;
 };
 
+/**
+ * Adds two numbers together...
+ * @param {Number} a 
+ * @param {Number} b 
+ * @returns {Number} a + b
+ */
 export const addUp = (a, b) => {
     return a + b;
 };
