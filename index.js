9e6a8bfa5e54a25e1ee04ae111a359ace34f15c6
Rick Craig <rick@spacervolver.com>
2022-06-08 20:11:10 +0100

    Add function added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export const testMe = (a, b) => {
     return a * b;
 };
+
+export const addUp = (a, b) => {
+    return a + b;
+};
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import assert from 'assert'
-import { testMe } from '../index.js'
+import { testMe, addUp } from '../index.js'
 
 describe("Test for testMe", () => {
     for (let i = 0; i < 10; i++) {
@@ -7,6 +7,16 @@ describe("Test for testMe", () => {
         const b = Math.ceil(Math.random()*10000)
         it(`should multiply ${a} with ${b} correctly`, () => {
             assert.equal(testMe(a, b), a * b)
+        })
+    }
+})
+
+describe("Test for addUp", () => {
+    for (let i = 0; i < 10; i++) {
+        const a = Math.ceil(Math.random()*10000)
+        const b = Math.ceil(Math.random()*10000)
+        it(`should add ${a} to ${b} correctly`, () => {
+            assert.equal(addUp(a, b), a + b)
         })
     }
 })
